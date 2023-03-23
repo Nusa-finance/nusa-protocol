2e351bd1a01cecbf56ab0d6f2cfb9ddea555ecb7
Ifan Wijaya <ifan.wijaya@bitcoin.co.id>
2023-03-23 15:35:29 +0700

    refactor

--- contracts-fixed-rate/JumpRateModelV3Storage.sol
+++ contracts-fixed-rate/JumpRateModelV3Storage.sol
@@ -16,7 +16,8 @@ contract JumpRateModelV3Storage{
     /**
      * @notice The approximate number of blocks per year that is assumed by the interest rate model
      */
-    uint public constant blocksPerYear = 2102400;
+    // uint public constant blocksPerYear = 2102400;
+    uint public constant blocksPerYear = 10512000;
 
     /**
      * @notice The multiplier of utilization rate that gives the slope of the interest rate
